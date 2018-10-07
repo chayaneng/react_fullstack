80d4f563d4419fcf1d0cfdaf93002f447dd9281b
chayaneng <chayaneng12@gmail.com>
2018-10-07 18:20:44 +0600

    Create hello.js
    I want to be your student Hello, I could not find you in udemy. Can you please give me your udemy link. I love you tutorials... I got your tutorial from one of my friend.
    But I want to be your student in udemy. can you please give me the link? Here is my email chayaneng12@gmail.com

---
+++ hello.js
@@ -0,0 +1,2 @@
+console.log('I want to be your student Hello, I could not find you in udemy. Can you please give me your udemy link. I love you tutorials... I got your tutorial from one of my friend.
+But I want to be your student in udemy. can you please give me the link? Here is my email chayaneng12@gmail.com');
